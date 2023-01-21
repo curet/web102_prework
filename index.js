b945487f59dcb70b80dd55b538fa42140c2100dc
José C.S. Curet <jcscuret@gmail.com>
2023-01-20 23:03:21 -0400

    add top funded to the DOM

--- index.js
+++ index.js
@@ -152,5 +152,9 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 let [firstGame, secondGame, ...remainingGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topFundedGame = document.createElement("p");
+topFundedGame.innerHTML = firstGame.name;
+const firstPosition = document.getElementById("first-game");
+firstPosition.append(topFundedGame)
 
 // do the same for the runner up item
