9432c41590db153ce1556892975b50be32de6d1f
José C.S. Curet <jcscuret@gmail.com>
2023-01-20 23:03:01 -0400

    get first and second place games

--- index.js
+++ index.js
@@ -149,6 +149,7 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [firstGame, secondGame, ...remainingGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
 
